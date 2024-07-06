0d161d58a84296bf779577158c191a3c6672462a
porimol roy <168266724+porimolr@users.noreply.github.com>
2024-07-06 22:06:19 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,200 @@
+//java 
+
+function dev_html(){
+   document.getElementById('html').innerHTML='> JavaScript.';
+
+}
+// function dev_css(){
+//    document.getElementById('css').innerHTML='> Cascading Style Sheets.'
+
+// }
+// function dev_cms(){
+//    document.getElementById('cms').innerHTML='> Content management system.'
+
+// }
+//ex_2
+function ad(){
+   document.getElementById("ng").src="images/bbb.png"; 
+   
+
+}
+function adn(){
+   document.getElementById("ng").src="images/ggg.png"; 
+   
+
+}
+//ex_3
+function t_color(){
+   document.getElementById("color").style.color="red";
+} 
+//ex_4
+function show(){
+   document.getElementById("text").style.display="block";
+} 
+function hide(){
+   document.getElementById("text").style.display="none";
+} 
+//ex_5
+function dw(){
+   document.write("WELCOME");
+} 
+//ex_6
+function wa(){
+  window.alert("Thank you..");
+} 
+//ex_7
+function ld(){
+  document.getElementById("td").innerHTML= 
+  Date();
+
+}
+//ex_8
+function fs(){
+  document.getElementById("f_a").style.fontSize='50px';
+
+} 
+//ex_9
+function clog(){
+   console.log("cosole");
+ } 
+ //ex_10
+   var number1 = 308;
+   var number2 = 308;
+   a = number1 + number2;
+
+function va(){
+   document.getElementById("v_a").innerHTML=
+   a;
+ } 
+
+
+ //ex_11
+
+   let nam1 , nam2 , result;
+   nam1 =100;
+   nam2 =308;
+   b = nam1+nam2;
+
+ function lv(){
+   document.getElementById("l_a").innerHTML=
+   b;
+ } 
+   
+  //ex_12
+
+  const po1 = 210;
+  const po2 = 111;
+  c = po1 + po2;
+
+function cv(){
+  document.getElementById("c_a").innerHTML=
+  c;
+} 
+
+  //ex_13
+
+  const dd1 = 'ssb ';
+  const pi = 616;
+  d = dd1 + pi;
+
+function dt(){
+  document.getElementById("d_a").innerHTML=
+  d;
+} 
+  //ex_14
+
+  const aa = 222;
+
+
+function ao(){
+  document.getElementById("a_a").innerHTML=
+  aa;
+}
+  //ex_15
+
+  let  jjj = 111;
+  let  kkk = 333;
+  let lll = jjj + kkk;
+
+
+function aao(){
+  document.getElementById("a_aa").innerHTML=
+  lll;
+}  
+  //ex_16
+
+  let  mmm = 5;
+  let  ooo = 5;
+  let m1 = mmm * ooo;
+
+
+function mo(){
+  document.getElementById("m_o").innerHTML=
+  m1;
+} 
+  //ex_17
+
+  var  aa1 = 55;
+  var  aa2 = 5;
+  var aoo = aa2 += aa1;
+
+
+function aaa(){
+  document.getElementById("aaa_o").innerHTML=
+  aoo;
+}
+//ex_18
+
+  let  co1 = 'A';
+  let  co2 = 'B';
+  let coo = co1 < co2;
+
+
+function co(){
+  document.getElementById("c_o").innerHTML=
+  coo;
+}
+//ex_19
+let  A1 = 3;
+let  A2 = 100;
+let  AA2 = (100 + 50) *A1;
+
+
+function Ao(){
+document.getElementById("A_o").innerHTML=
+AA2;
+}
+//ex_20
+let  as1 = 2 + 2 ;
+let  as2 = "<br> 9" + 9;
+let  as3 = "<br>Hello" + 5;
+let  as4 =  as1 + as2 + as3;
+
+
+function As(){
+document.getElementById("A_s").innerHTML=
+as4;
+}
+//ex_21
+let  do1 = 200;
+let  do2 = 2;
+let  do3 =  do1 / do2 ;
+
+
+function doo(){
+document.getElementById("d_o").innerHTML=
+do3;
+}
+//ex_22
+let  so1 = 100;
+let  so2 = 40;
+let  so3 =  so1 - so2 ;
+
+
+function so(){
+document.getElementById("s_o").innerHTML=
+so3;
+}
+
+ 
+ 
